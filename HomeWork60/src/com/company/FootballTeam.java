38082825123d362eef54d5b1fc7e6fae4bbc44ea
sa6yrov <sabyrov.aidin@gmail.com>
2020-03-04 12:00:56 +0600

    first commit

---
+++ HomeWork60/src/com/company/FootballTeam.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Random;
+
+public class FootballTeam implements Runnable{
+//    private int goals;
+//    private int yellowCard;
+//    private int inOffside;
+//
+//    public FootballTeam(int goals, int yellowCard, int inOffside) {
+//        this.goals = goals;
+//        this.yellowCard = yellowCard;
+//        this.inOffside = inOffside;
+//    }
+
+    @Override
+    public  void run() {
+//        System.out.println(Thread.currentThread().getName() + "\n" + "Количество забитых голов: " + goals + "\n" +
+//                "Желтые карточки: " + yellowCard + "\n" + "Офсайды: " + inOffside);
+        Random rnd = new Random();
+        System.out.println(Thread.currentThread().getName() + "\n" + "Количество забитых голов: " + (Main.goals + rnd.nextInt(15)) +
+                "\n" + "Желтые карточки: " + (Main.yellowCard + rnd.nextInt(15)) + "\n" + "Офсайды: " + (Main.inOffside + rnd.nextInt(15)));
+    }
+}
